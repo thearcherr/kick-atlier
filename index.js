8a32fdbf07fbd527bffece1eb06ca4edc7ca8de5
Archer <53667566+thearcherr@users.noreply.github.com>
2025-01-20 17:33:57 +0500

    error fixing

--- index.js
+++ index.js
@@ -14,14 +14,17 @@ const __dirname = path.dirname(__filename);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'src')));
 
-// Set view engine
+app.set('views', path.join(__dirname, 'views')); // Absolute path to "views" directory
 app.set('view engine', 'ejs');
+
+// Serve static files from the "src" directory
+app.use(express.static(path.join(__dirname, 'src')));
 
 // Routes
 app.get('/', (req, res) => {
     res.render('index');
 });
-
+ 
 // Start server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
